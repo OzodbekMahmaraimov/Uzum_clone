dd2d5ee496b530437c9f2924f50fb61f1bdea3ae
OzodbekMahmaraimov <ozodebkmahmaraimov07@gmail.com>
2024-02-10 13:24:57 +0500

    salom

--- src/components/Home/saralanganlar/Saralangan.jsx
+++ src/components/Home/saralanganlar/Saralangan.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from '../Navbar'
+import Navsm from '../Navsm'
+import Sara from './saralangan.png'
+import './saralangan.css'
 
 const Saralangan = () => {
   return (
-    <div>
-        Asilbek
+    <div className='container mt-4'>
+        <Navbar />
+        <hr className='p-0 m-0' />
+        <div className=' d-flex like-container gap-2'>
+          <img className='likeImg' src={Sara} alt="saralangan img" />
+          <h2>Sizga yoqqanini qo'shing</h2>
+          <p>Mahsulotdagi ♡ belgisini bosing. Akkauntga kiring va barcha saralanganlar saqlanib qoladi</p>
+          <button>Akkauntga kirish</button>
+        </div>
+        <Navsm />
     </div>
   )
 }
